3ea32e07beb9ae984ec840d23806e1b6e7291609
Diallo Amidou <diallo.amidou24@yahoo.fr>
2024-04-03 11:59:08 +0200

    save work
    chiffrage / dechifrage mdp dans user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -24,6 +24,12 @@ export const getUserById = async (req, res) => {
 export const createUser = async (req, res) => {
     try {
         const user = await User.create(req.body);
+
+        // Chiffrer le mot de passe
+        user.password = await bcrypt.hash(user.password, process.env.SALT_ROUNDS);
+        await user.save();
+        const response = await User.create(user);
+                          
         res.status(201).json(user);
     } catch (error) {
         res.status(500).json({ error: 'Internal server error' });
@@ -32,9 +38,23 @@ export const createUser = async (req, res) => {
 
 export const updateUserById = async (req, res) => {
     try {
-        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
+
+        const updatedUser = req.body;
+        const user = await User.findById(req.body._id);
+
+        
         if (!user) {
             return res.status(404).json({ error: 'User not found' });
+        }
+
+        // Si le mot de passe est inclus dans la requête, le chiffrer
+        if (bcrypt.compare(updatedUser.password, user.password)) {
+            const hashedPassword = await bcrypt.hash(password, process.env.SALT_ROUNDS);
+            req.body.password = hashedPassword;
+
+            const response = await User.findByIdAndUpdate(req.body._id, req.body);
+        }else{
+            return res.status(401).json({ error: 'Invalid password' });
         }
         res.json(user);
     } catch (error) {
